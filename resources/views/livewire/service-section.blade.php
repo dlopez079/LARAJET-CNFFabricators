cb1e13fa481f6a3e40466121e3df4536d9b49340
David Lopez <dlopez@mattmen.com>
2022-02-23 11:56:04 -0500

    Modified Service Section Blade.

--- resources/views/livewire/service-section.blade.php
+++ resources/views/livewire/service-section.blade.php
@@ -1,3 +1,3 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-</div>
+<section id="service" class="block border border-black bg-decoration-yellow-300">
+    <h1>SERVICE SECTION</h1>
+</section>
